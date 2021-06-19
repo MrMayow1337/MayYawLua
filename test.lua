775481e90eded4ce84b8384d58ddbd4232eb1067
MrMayow1337 <82043326+MrMayow1337@users.noreply.github.com>
2021-06-19 12:36:38 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 ScriptName=GetScriptName()
 LocalVersion=1.3.5
 LastVersion= string.gsub(http.Get("https://raw.githubusercontent.com/MrMayow1337/MayYawLua/main/Version.txt"), "\n", "")
-Script=string.gsub(http.Get("https://raw.githubusercontent.com/MrMayow1337/MayYawLua/main/Version.txt"), "\n", "")
+Script=string.gsub(http.Get("https://raw.githubusercontent.com/MrMayow1337/MayYawLua/main/test.lua"), "\n", "")
 if LocalVersion~=LastVersion then
 	file.Delete(ScriptName)
 	file.Open(ScriptName,w)
